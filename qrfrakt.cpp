db66b70e2e9330344d2672624d44a2467f8bc10e
Ingo Ruhnke <grumbel@gmail.com>
2015-04-22 16:19:44 +0200

    Center the thing

--- qrfrakt.cpp
+++ qrfrakt.cpp
@@ -7,13 +7,15 @@
 #include <math.h>
 #include <stdint.h>
 
-void generate_rfrakt(QImage& image, float z)
+void generate_rfrakt(QImage& image, double z)
 {
   for(int y = 1; y < image.height(); ++y)
   {
     for(int x = 0; x < image.width(); ++x)
     {
-      double px = sin(x * y * z);
+      double xi = ((static_cast<double>(x) / image.width()) - 0.5) * z;
+      double yi = ((static_cast<double>(y) / image.height()) - 0.5) * z;
+      double px = sin(xi * yi);
       uint8_t c = static_cast<int>(((px + 1.0)/2.0) * 255);
       image.setPixel(x, y, qRgb(c, c, c));
     }
@@ -24,7 +26,7 @@ int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
 
-  double z = 1000.01;
+  double z = 1.5;
 
   QImage image(512, 512, QImage::Format_RGB32);
   generate_rfrakt(image, z);
@@ -36,7 +38,7 @@ int main(int argc, char** argv)
   QTimer timer;
   QObject::connect(&timer, &QTimer::timeout,
                    [&]{
-                     z += 0.001;
+                     z *= 1.01;
                      generate_rfrakt(image, z);
                      label.setPixmap(QPixmap::fromImage(image));
                    });
